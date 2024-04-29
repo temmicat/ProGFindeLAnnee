48cc990ea2376c543c155d95210175be70c211c8
LazyLittleCreature <lazylittlecreature@outlook.com>
2024-04-29 14:57:03 +0200

    Initial commit

---
+++ Assets/Scripts/CharacterMove.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CharacterMove : MonoBehaviour
+{
+    private float _moveSpeed = 5f;
+    private float _moveAxis;
+    
+    void FixedUpdate()
+    {
+        transform.Translate(new Vector3(1, 0, 0) * (_moveAxis * _moveSpeed * Time.deltaTime));
+    }
+
+    public void HandleMove(InputAction.CallbackContext moveInput)
+    {
+        _moveAxis = moveInput.ReadValue<float>();
+    }
+}
